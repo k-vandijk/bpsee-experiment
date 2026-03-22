e97c7245401afa22d6399624511b9fd8c57692a4
Kevin van Dijk <kevinvd05@gmail.com>
2026-03-22 14:50:53 +0100

    Add project files.

---
+++ BPSEE.Experiment.Domain/DependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace BPSEE.Experiment.Domain;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddDomain(this IServiceCollection services)
+    {
+        return services;
+    }
+}
---
+++ BPSEE.Experiment.Microservices.Orders/Program.cs
@@ -0,0 +1,25 @@
+using BPSEE.Experiment.Application;
+using BPSEE.Experiment.Domain;
+using BPSEE.Experiment.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddApplication();
+builder.Services.AddDomain();
+builder.Services.AddInfrastructure();
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
